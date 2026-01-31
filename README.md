8ddb3b80514318bcb8fa8a667ec6f8bf60fce473
Jhoan Buitrago <jhoanuitrago@gmail.com>
2026-01-31 17:41:50 -0500

    Improve docs

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
 # GodotGreetd
-GDExtension for the Greetd IPC Protocol
+---
+A GDExtension for the [Greetd](https://man.sr.ht/~kennylevinsen/greetd/) IPC Protocol.
 
-## Development
+Create **greeters** (login applications) using the [Godot game engine](https://godotengine.org/).
 
-### Setup
-1. Initialize the godot-cpp git submodule via `git submodule update --init`. (Currently using Godot 4.5 branch)
-2. Install scons: `sudo pacman -S scons`
-3. Install a c++ compiler like clang (or g++): `sudo pacman -S clang`
-4. Compile the gdextension: `scons`
+**Is this a good idea?** I don't know, but it's fun :)
 
-### Wayland
-To make your Godot project use Wayland:
-1. Open "Project" -> "Project Settings..."
-2. Enable "Advanced Settings" on the top right.
-3. Go to "Display" -> "Display Server" -> "Driver.linuxbsd" -> "wayland".
+# Development
 
-### Configuring an IDE or language server
+## Setting up the environment
+1. Clone the repo and cd into it.
+2. Initialize the godot-cpp git submodule via `git submodule update --init`.
+	- Currently using Godot 4.5 branch but you can probably use an older 4.x version if needed.
+	- You can configure this in the `.gitmodules` file.
+3. Install scons: `sudo pacman -S scons`
+	- You should be able to use CMake but I'm not familiar with it.
+4. Install a c++ compiler like clang (or use g++): `sudo pacman -S clang`
+5. Compile the gdextension: `scons`
+
+## Configuring an IDE or language server
 To work with an IDE or language server, you can use a compilation database file called `compile_commands.json`. Most IDEs (and clangd language server) should automatically identify this file, and self-configure appropriately.
 ```sh
 # Generate compile_commands.json while compiling
@@ -28,14 +31,58 @@ scons compiledb=yes compile_commands.json
 scons compiledb=yes use_llvm=true
 ```
 
+## Creating your greeter
+This extension simply implements the Greetd IPC Protocol. Your greeter still needs to use the protocol appropiately.
+
+A typical login flow looks like this:
+1. Create a session for a user.
+2. Answer an authenthication message (usually the user's password)
+3. Start the session with a command (usually running a wayland session)
+4. Exit the greeter app.
+
+After the greeter closes, greetd will log in the user and launch the command.
+
+You can see an example in `demo/login_screen.gd`
+
+The extension also provide some useful utilities for building your greeter:
+- Get wayland sessions (e.g. Plasma, Hyprland, etc)
+	- This uses a simple parsing of files in "/usr/share/wayland-sessions/". Good enough for now.
+	- TO DO: X11 sessions.
+- Get users.
+
+TODO: pending an in-depth explanation of the protocol and some diagrams.
+
+## Testing your greeter
+The `server.py` file implements a simple Greetd server with mocked data that connects to the `/tmp/example_socket` UNIX socket.
+
+Run it with `python server.py`.
+
+The extension looks for a socket in the `GREETD_SOCK` env variable. If it's empty it connects to `/tmp/example_socket`. This is a hack for testing. I'll probably change it in the future.
+
+## Using your greeter
+First, [install greetd](https://sr.ht/~kennylevinsen/greetd/#installation).
+
+TODO: finish this part
+
+When your computer starts it will use the "greeter" user to launch the greeter app.
+Godot wants to create and save some files in the user directory. The problem is that the greeter user does not have a home directory, so we will need to create some directories with the proper permissions.
+```sh
+sudo mkdir -p /home/greeter/.cache
+sudo mkdir -p /home/greeter/.config/dconf
+sudo mkdir -p /home/greeter/.local
+
+sudo chown -R greeter:greeter /home/greeter/
+
+# You may need to give permissions:
+sudo chmod -R 700 /home/greeter
+```
+
 # Notes to self
 
 ## Usage - Template
-
 * register the classes you want Godot to interact with inside the `register_types.cpp` file in the initialization method (here `initialize_gdextension_types`) in the syntax `GDREGISTER_CLASS(CLASS-NAME);`.
 
 ## Usage - Actions
-
 This repository comes with a GitHub action that builds the GDExtension. It triggers automatically for each pushed change. You can find and edit it in [builds.yml](.github/workflows/builds.yml).
 After a workflow run is complete, you can find the file `godot-cpp-template.zip` on the `Actions` tab on GitHub.
 
--- demo/login_screen.gd
+++ demo/login_screen.gd
@@ -8,22 +8,25 @@ func _ready() -> void:
 	var err := config.load(CONFIG_PATH)
 	if err != Error.OK:
 		printerr("Failed to open config.cfg")
-		
+
 	print(config.encode_to_text())
-		
+
 	_init_sessions()
 	_init_users()
-	
+
 	%Password.text_submitted.connect(_on_password_submitted)
 	%LogIn.pressed.connect(login)
 	%SessionSelect.item_selected.connect(_on_session_selected)
-	%Password.grab_focus()
+
+	%Password.grab_focus() # Automatically focus the password field for quick login.
+
 	get_tree().set_auto_accept_quit(false) # Handle quit manually
 
 
+# Get a list of users and select the last user that logged in.
 func _init_users():
 	var last_user = config.get_value("General", "last_user", "")
-	
+
 	var users := greeter.get_users()
 	for user in users:
 		%Username.add_item(user)
@@ -31,9 +34,10 @@ func _init_users():
 			%Username.select(-1)
 
 
+# Get a list of sessions and select the last used session.
 func _init_sessions():
 	var last_session = config.get_value("General", "last_session", "")
-	
+
 	var sessions := greeter.get_wayland_sessions()
 	var index = 0
 	for session in sessions:
@@ -44,20 +48,24 @@ func _init_sessions():
 			%SessionSelect.select(%SessionSelect.item_count - 1)
 
 
+# Save the current selected session as the last one.
 func _on_session_selected(_index: int):
 	var session = %SessionSelect.text
 	config.set_value("General", "last_session", session)
 
 
+# Save the selected user as the last one.
 func _on_user_selected(_index: int):
 	var user = %Username.text
 	config.set_value("General", "last_user", user)
 
 
+# When pressing "Enter" run the Login
 func _on_password_submitted(_password):
 	login()
-	
 
+
+# Manually handle app closing.
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_WM_CLOSE_REQUEST:
 		_quit()
@@ -67,67 +75,68 @@ func login() -> void:
 	var username: String = %Username.text
 	var password: String = %Password.text
 	var response := greeter.create_session(username)
-	
+
 	if response is GreetdError:
 		%Alert.text = response.get_error_description()
-		log_info(response.get_error_description())
+		_log_info(response.get_error_description())
 		return
 	elif response is GreetdAuthMessage:
-		log_info("%s - %s" % [response.get_auth_message_type(), response.get_auth_message()])
+		_log_info("%s - %s" % [response.get_auth_message_type(), response.get_auth_message()])
 	else:
 		# TODO if the response is a success then we can start the session
 		# How does that happen? Users without passwords?
 		pass
-	
-	log_info("Session created") 
+
+	_log_info("Session created")
 	response = greeter.answer_auth_message(password)
 	if response is GreetdError:
 		%Alert.text = response.get_error_description()
-		log_info(response.get_error_description())
+		_log_info(response.get_error_description())
 		cancel_session()
 		return
 	elif response is GreetdAuthMessage:
-		log_info("Unexpected: %s - %s" % [response.get_auth_message_type(), response.get_auth_message()])
+		_log_info("Unexpected: %s - %s" % [response.get_auth_message_type(), response.get_auth_message()])
 		cancel_session()
 		return
 	else:
-		log_info("Auth answered")
-	
+		_log_info("Auth answered")
+
 	var cmd = %SessionSelect.get_selected_metadata()
 	response = greeter.start_session(cmd)
 	if response is GreetdError:
 		%Alert.text = response.get_error_description()
-		log_info(response.get_error_description())
+		_log_info(response.get_error_description())
 		cancel_session()
 		return
 	elif response is GreetdAuthMessage:
-		log_info("Unexpected: %s - %s" % [response.get_auth_message_type(), response.get_auth_message()])
+		_log_info("Unexpected: %s - %s" % [response.get_auth_message_type(), response.get_auth_message()])
 		cancel_session()
 		return
 	else:
-		log_info("Session started")
-	
+		_log_info("Session started")
+
 	if OS.has_feature("editor"):
 		%Alert.text = "Success :D Exiting in 3 seconds"
 		await get_tree().create_timer(3.0).timeout
-	
+
 	_quit()
 
 
+# Save config and quit
 func _quit() -> void:
 	config.save(CONFIG_PATH)
 	get_tree().quit()
 
 
-func log_info(text: String) -> void:
+func _log_info(text: String) -> void:
 	%Log.add_text(text + "\n")
 
 
 func cancel_session() -> void:
 	var response := greeter.cancel_session()
 	if response is GreetdError:
-		log_info(response.get_error_description())
+		_log_info(response.get_error_description())
 	elif response is GreetdAuthMessage:
-		log_info("Unexpected: %s - %s" % [response.get_auth_message_type(), response.get_auth_message()])
+		_log_info("Unexpected: %s - %s" % [response.get_auth_message_type(), response.get_auth_message()])
 	else:
-		log_info("Cancelled successfully")
+		_log_info("Cancelled successfully")
